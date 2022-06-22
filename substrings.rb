1f2bb7daafd27d6b1fe40086f0b1fc309f117a9e
Sarah Dylan <SarahmDylan@gmail.com>
2022-06-22 13:19:20 -0700

    Replace map with reduce method

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,8 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  word_count_array = 
-    string.downcase.split.map do |word| 
-      dictionary.select { |test| word.include?(test) }
-    end
-      .flatten
-
-  word_count_array.reduce(Hash.new(0)) { |hash, word| hash[word] += 1; hash}
+  string.downcase.split.reduce(Hash.new(0)) do |hash, word| 
+    dictionary.select { |test| hash[test] += 1 if word.include?(test) }
+    hash
+  end
 end
